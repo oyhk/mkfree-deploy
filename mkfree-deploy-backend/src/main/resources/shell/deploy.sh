5134ed4df087424c81ff4b2c171cbd73c11231c1
oyhk <381895649@qq.com>
2017-01-24 19:57:26 +0800

    完成git 的release 分支拉代码

--- mkfree-deploy-backend/src/main/resources/shell/deploy.sh
+++ mkfree-deploy-backend/src/main/resources/shell/deploy.sh
@@ -5,13 +5,15 @@ localPath=$2 #项目路径
 gitUrl=$3 #git仓库地址
 publishBranch=$4 #发布分支
 
+#git@git.oschina.net:kcoupon/rockcent.git 截取项目名称 rockcent
+gitProjectName=${gitUrl##*/} #git项目名称
 
 echo 'deploy start'
-projectPath="$localPath/$projectName"
-echo ${projectPath}
+projectAllPath=${localPath}/${projectName}/${gitProjectName/.git/''}
+projectPath=${localPath}/${projectName}
 # 当项目不存在
-if [ ! -d "$projectPath" ]; then
-    mkdir -p ${projectPath}
+if [ ! -d "$projectAllPath" ]; then
+    mkdir -p ${projectAllPath}
     cd ${projectPath}
     echo "git clone $gitUrl"
     echo 'git clone start . . .'
@@ -21,12 +23,21 @@ if [ ! -d "$projectPath" ]; then
 fi
 
 # 进入指定目录
-echo "cd $projectPath"
-cd ${projectPath}
+echo "cd $projectAllPath"
+cd ${projectAllPath}
 
-
-
-remoteBranch=echo git branch -a | grep remotes | grep ${publishBranch}
+git pull
+if [ 'release' == ${publishBranch} ]; then
+    # 获取git release 分支列表
+    remoteBranch=$(git branch -a|grep remotes|grep release)
+    # 获取release 分支 最后一个版本
+    lastReleaseBranch=${remoteBranch##* }
+    git checkout ${lastReleaseBranch}
+    remoteBranchVersion=$(git rev-parse HEAD)
+    echo "git checkout  $lastReleaseBranch , version is : $remoteBranchVersion"
+    # 用maven 编译 java 项目
+    mvn clean package -pl rockcent-common-lib,rockcent-mall-lib,rockcent-site-api -am
+fi
 
 
 echo 'deploy finish !!!'
