0017afaf4d2a6216e411f0c1155de48314156fc1
oyhk <381895649@qq.com>
2017-02-08 15:42:09 +0800

    fix

--- mkfree-deploy-backend/src/main/java/com/mkfree/deploy/controller/ProjectController.java
+++ mkfree-deploy-backend/src/main/java/com/mkfree/deploy/controller/ProjectController.java
@@ -19,7 +19,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.data.domain.Page;
@@ -215,7 +214,11 @@ public class ProjectController extends BaseController {
                 return;
             }
 
-            String deployShellPath = new ClassPathResource("shell/deploy.sh").getFile().getPath();
+            Resource resource = resourceLoader.getResource("classpath*:/shell/deploy.sh");
+
+            log.info(resource.getFilename());
+            log.info(resource.getFile().toString());
+            String deployShellPath = resource.getFile().getPath();
 
 //            String deployShellPath = "/Users/oyhk/rockcent/project/mkfree-deploy/mkfree-deploy-backend/src/main/resources/shell/deploy.sh";
             ShellHelper.SINGLEONE.executeShellCommand(log, "chmod u+x " + deployShellPath);
@@ -256,7 +259,7 @@ public class ProjectController extends BaseController {
             for (ServerMachine serverMachine : serverMachineList) {
 
                 //构建前命令
-                List<ProjectStructureStep> projectStructureStepBeforeList = projectStructureStepRepository.findByProjectIdAndTypeAndProjectEnvConfigId(project.getId(), ProjectStructureStepType.BEFORE, projectEnvConfig.getId());
+                List<ProjectStructureStep> projectStructureStepBeforeList = projectStructureStepRepository.findByProjectIdAndTypeAndProjectEnvConfigId(project.getId(), ProjectStructureStepType.BEFORE,projectEnvConfig.getId());
                 StringBuilder projectStructureStepBeforeBuilder = new StringBuilder();
                 if (projectStructureStepBeforeList.size() > 0) {
                     projectStructureStepBeforeList.forEach(projectStructureStep -> {
@@ -266,7 +269,7 @@ public class ProjectController extends BaseController {
                 }
 
                 // 构建后命令
-                List<ProjectStructureStep> projectStructureStepAfterList = projectStructureStepRepository.findByProjectIdAndTypeAndProjectEnvConfigId(project.getId(), ProjectStructureStepType.AFTER, projectEnvConfig.getId());
+                List<ProjectStructureStep> projectStructureStepAfterList = projectStructureStepRepository.findByProjectIdAndTypeAndProjectEnvConfigId(project.getId(), ProjectStructureStepType.AFTER,projectEnvConfig.getId());
                 StringBuilder projectStructureStepAfterBuilder = new StringBuilder();
                 if (projectStructureStepAfterList.size() > 0) {
                     projectStructureStepAfterList.forEach(projectStructureStep -> {
