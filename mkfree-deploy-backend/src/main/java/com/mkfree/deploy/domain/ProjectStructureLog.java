211aa3680dbe460af46524481d8cca1571fda536
oyhk <381895649@qq.com>
2017-02-09 15:40:13 +0800

    fix

--- mkfree-deploy-backend/src/main/java/com/mkfree/deploy/domain/ProjectStructureLog.java
+++ mkfree-deploy-backend/src/main/java/com/mkfree/deploy/domain/ProjectStructureLog.java
@@ -10,25 +10,25 @@ import javax.persistence.Entity;
 @Entity
 public class ProjectStructureLog extends IDEntity {
 
-    @Column(columnDefinition = "varchar (100) comment '构建名'", unique = true)
+    @Column(columnDefinition = "varchar (100) comment '构建名'")
     private String name;
 
     @Column(columnDefinition = "bigint comment '用户id'")
     private Long userId;
 
-    @Column(columnDefinition = "varchar (100) comment '用户名'", unique = true)
+    @Column(columnDefinition = "varchar (100) comment '用户名'")
     private String username;
 
     @Column(columnDefinition = "bigint comment '项目id'")
     private Long projectId;
 
-    @Column(columnDefinition = "varchar (100) comment '项目名'", unique = true)
+    @Column(columnDefinition = "varchar (100) comment '项目名'")
     private String projectName;
 
     @Column(columnDefinition = "bigint comment '构建序列号，递增'")
     private Long seqNo;
 
-    @Column(columnDefinition = "longtext comment '用户id'")
+    @Column(columnDefinition = "longtext comment '详细日志'")
     private String description;
 
     public Long getUserId() {
