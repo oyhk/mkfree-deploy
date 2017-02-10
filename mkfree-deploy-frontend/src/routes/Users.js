f9d233a04ba15dc293fb21d27ed927ae552920d4
wzmin <370583587@qq.com>
2017-02-10 14:53:31 +0800

    fix

--- mkfree-deploy-frontend/src/routes/Users.js
+++ mkfree-deploy-frontend/src/routes/Users.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {connect} from 'dva';
-import {Table, Pagination, Popconfirm, Button} from 'antd';
-import {Link, browserHistory, routerRedux} from 'dva/router';
+import { connect } from 'dva';
+import { Table, Pagination, Popconfirm, Button, message } from 'antd';
+import { Link, browserHistory, routerRedux } from 'dva/router';
 import styles from './Users.less';
 import {
   PAGE_SIZE,
@@ -12,12 +12,35 @@ import UserModal from '../components/Users/UserModal';
 
 function Users({dispatch, list: dataSource, loading, total, pageNo: current, visible}) {
 
-  function deleteHandler(id) {
-    dispatch({
-      type: 'users/remove',
-      payload: id,
-    });
+  let deleteUserLock = false;
+
+  function deleteUser(id) {
+    if (!deleteUserLock) {
+      deleteUserLock = true;
+      dispatch({
+        type: `users/userDelete`,
+        payload: {
+          id,
+        },
+        callBack: (result)=> {
+          if (result.code === '1') {
+            message.success('删除成功');
+            dispatch({
+              type: 'users/fetch',
+              payload: {
+                pageNo: 1,
+              }
+            })
+          } else {
+            deleteUserLock = false;
+          }
+        }
+      });
+    } else {
+      message.warning('请勿连续点击按钮');
+    }
   }
+
 
   function pageChangeHandler(pageNo) {
     dispatch(routerRedux.push({
@@ -53,7 +76,7 @@ function Users({dispatch, list: dataSource, loading, total, pageNo: current, vis
       render: (text, record) => (
         <span className={styles.operation}>
           <Link to={`${ROUTE_ADMIN_USERS_INFO}/${record.id}`}>编辑</Link>
-          <Popconfirm title="Confirm to delete?" onConfirm={deleteHandler.bind(null, record.id)}>
+          <Popconfirm title="Confirm to delete?" onConfirm={()=> deleteUser(record.id)}>
             <a href="">删除</a>
           </Popconfirm>
         </span>
