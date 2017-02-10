9605f4ae8502ba78a9ed49fc7e661a36bac2f556
wzming <1532142620@qq.com>
2017-02-10 16:16:59 +0800

    项目

--- mkfree-deploy-frontend/src/models/serverMachine.js
+++ mkfree-deploy-frontend/src/models/serverMachine.js
@@ -1,54 +1,54 @@
-import * as projectService from '../services/serverMachine';
-import {ROUTE_ServerMachine} from '../constants';
+import * as projectService from "../services/serverMachine";
+import {ROUTE_ServerMachine} from "../constants";
 
 export default {
-  namespace: 'serverMachine',
-  state: {
-    list: [],
-    total: null,
-    pageNo: null,
-  },
-  reducers: {
-    save(state, {payload: {data: list, total, pageNo}}) {
-      return {...state, list, total, pageNo};
+    namespace: 'serverMachine',
+    state: {
+        list: [],
+        total: null,
+        pageNo: null,
     },
-  },
-  effects: {
-    *fetch({payload: {pageNo = 1}}, {call, put}) {
-      const result = yield call(projectService.fetch, {pageNo});
-      yield put({
-        type: 'save',
-        payload: {
-          data: result.list,
-          total: result.totalCount,
-          pageNo: result.pageNo,
+    reducers: {
+        save(state, {payload: {data: list, total, pageNo}}) {
+            return {...state, list, total, pageNo};
         },
-      });
     },
-    *patch({payload: values}, {call, put}) {
-      yield call(projectService.update, values);
-      yield put({type: 'reload'});
-    },
-    *remove({payload: values}, {call, put}) {
-      yield call(projectService.remove, values);
-      yield put({type: 'reload'});
-    },
-    *create({payload: values}, {call, put}) {
-      yield call(projectService.save, values);
-      yield put({type: 'reload'});
-    },
-    *reload(action, {put, select}) {
-      const pageNo = yield select(state => state.serverMachine.pageNo);
-      yield put({type: 'fetch', payload: {pageNo}});
-    }
-  },
-  subscriptions: {
-    setup({dispatch, history}) {
-      return history.listen(({pathname, query}) => {
-        if (pathname === ROUTE_ServerMachine) {
-          dispatch({type: 'fetch', payload: query});
+    effects: {
+        *fetch({payload: {pageNo = 1}}, {call, put}) {
+            const result = yield call(projectService.fetch, {pageNo});
+            yield put({
+                type: 'save',
+                payload: {
+                    data: result.list,
+                    total: result.totalCount,
+                    pageNo: result.pageNo,
+                },
+            });
+        },
+        *patch({payload: values}, {call, put}) {
+            yield call(projectService.update, values);
+            yield put({type: 'reload'});
+        },
+        *remove({payload: values}, {call, put}) {
+            yield call(projectService.remove, values);
+            yield put({type: 'reload'});
+        },
+        *create({payload: values}, {call, put}) {
+            yield call(projectService.save, values);
+            yield put({type: 'reload'});
+        },
+        *reload(action, {put, select}) {
+            const pageNo = yield select(state => state.serverMachine.pageNo);
+            yield put({type: 'fetch', payload: {pageNo}});
         }
-      });
     },
-  },
+    subscriptions: {
+        setup({dispatch, history}) {
+            return history.listen(({pathname, query}) => {
+                if (pathname === ROUTE_ServerMachine) {
+                    dispatch({type: 'fetch', payload: query});
+                }
+            });
+        },
+    },
 };
