04a8d5ae5b5d671bd0c4c7d250fda4ef7d9021d1
oyhk <381895649@qq.com>
2017-02-15 17:19:07 +0800

    fix

--- mkfree-deploy-backend/src/main/java/com/mkfree/deploy/domain/ProjectDeployFile.java
+++ mkfree-deploy-backend/src/main/java/com/mkfree/deploy/domain/ProjectDeployFile.java
@@ -14,9 +14,9 @@ import javax.persistence.Enumerated;
 @Entity
 public class ProjectDeployFile extends IDEntity {
 
-    @Column(columnDefinition = "varchar (100) comment '本地文件路劲'", unique = true)
+    @Column(columnDefinition = "varchar (100) default '' comment '本地文件路劲'", unique = true)
     private String localFilePath;
-    @Column(columnDefinition = "varchar (255) comment '远程服务文件路劲'", nullable = false)
+    @Column(columnDefinition = "varchar (255) default '' comment '远程服务文件路劲'", nullable = false)
     private String remoteFilePath;
     @Column(columnDefinition = "bigint comment '项目id'")
     private Long projectId;
