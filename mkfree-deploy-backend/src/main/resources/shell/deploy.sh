02c6ae6a06bf44dc405d62ef798f99a14ca0ad1d
oyhk <381895649@qq.com>
2017-03-17 11:51:34 +0800

    fix

--- mkfree-deploy-backend/src/main/resources/shell/deploy.sh
+++ mkfree-deploy-backend/src/main/resources/shell/deploy.sh
@@ -71,7 +71,7 @@
     IFS=';' read -ra ADDR <<< "$structureStepBeforeList"
     for i in "${ADDR[@]}"; do
         echo "$i"
-        echo ${i}
+        ${i}
     done
     ################ 构建步骤 end  ############
 
