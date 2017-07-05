7e9a3406fe3af47032eac76267cd73bf83f77dde
oyhk <381895649@qq.com>
2017-07-05 10:24:37 +0800

    重新改造前端，之前的代码有点烂- -

--- mkfree-deploy-frontend/src/router.js
+++ mkfree-deploy-frontend/src/router.js
@@ -1,53 +1,22 @@
-import React from "react";
-import {Router, Route, IndexRedirect} from "dva/router";
-import MainLayout from "./components/Mainlayout/MainLayout";
-import Header from "./components/Mainlayout/Header";
-import Users from "./routes/Users";
-import Projects from "./routes/Projects";
-import ProjectsHeader from "./components/Projects/ProjectsHeader";
-import ProjectsCreate from "./routes/ProjectsCreate";
-import ServerMachine from "./routes/ServerMachine";
-import UsersInfo from "./routes/UserInfo";
-import SsoIndex from "./routes/SsoIndex";
-import StructureLogs from "./routes/StructureLogs";
-import {
-    ROUTE_USERS,
-    ROUTE_PROJECTS,
-    ROUTE_PROJECTS_CREATE,
-    ROUTE_PROJECTS_INFO,
-    ROUTE_ServerMachine,
-    ROUTE_USERS_INFO,
-    LOGS_LIST,
-    ROUTE_USERS_SIGN_IN,
-    ROUTE_JOB,
-    ROUTE_USERS_CREATE,
-    ROUTE_PROJECT_STRUCTURE_LOGS
-} from "./constants";
+/* eslint-disable react/jsx-space-before-closing */
+import React from 'react';
+import {Router, Route} from 'dva/router';
+import {MainLayout, ProjectRoute, SignInRoute} from './routes';
 
-export default function ({history}) {
+
+const route_prefix = '';
+const route_sign_in = `${route_prefix}/sign_in`;
+const route_project = `${route_prefix}/project`;
+
+
+function RouterConfig({history}) {
     return (
         <Router history={history}>
             <Route path="/" component={MainLayout}>
-                <IndexRedirect to={ROUTE_PROJECTS}/>
-                <Route path="/" component={Header}>
-                    {/*admin*/}
-                    <Route path={ROUTE_USERS} component={Users}/>
-                    <Route path={ROUTE_USERS_CREATE} component={UsersInfo}/>
-                    <Route path={ROUTE_USERS_INFO + '/:id'} component={UsersInfo}/>
-                    {/*project*/}
-                    <Route path={ROUTE_PROJECTS} component={Projects}/>
-                    <Route path={ROUTE_PROJECTS_CREATE} component={ProjectsCreate}/>
-                    {/*serverMachine*/}
-                    <Route path={ROUTE_ServerMachine} component={ServerMachine}/>
-                    {/*projectStructureLog*/}
-                    <Route path={ROUTE_PROJECT_STRUCTURE_LOGS} component={StructureLogs}/>
-                </Route>
-                <Route path={ROUTE_PROJECT_STRUCTURE_LOGS + '/:project_name' + '/:id'} component={ProjectsHeader}>
-                    <Route path={ROUTE_PROJECTS_INFO} component={ProjectsCreate}/>
-                    <Route path={LOGS_LIST + '/:info_id'} component={StructureLogs}/>
-                </Route>
+                <Route path="deploy/project" component={ProjectRoute}/>
             </Route>
-            <Route path={ROUTE_USERS_SIGN_IN} component={SsoIndex}/>
         </Router>
     );
-};
+}
+
+export default RouterConfig;
--- mkfree-deploy-frontend/src/utils/request.js
+++ mkfree-deploy-frontend/src/utils/request.js
@@ -1,90 +1,58 @@
-import fetch from "dva/fetch";
-import cookie from "react-cookie";
-import {ROUTE_PREFIX,ROUTE_USERS_SIGN_IN} from "../constants";
-import {browserHistory} from "dva/router";
-
-
-const apiDomains = {
-  dev: `http://192.168.1.210:8091`,
-  // dev: 'http://192.168.3.133:8090', // HK 电脑
-  prod: ''//当为空时，api就是相对路径
-};
+import fetch from 'dva/fetch';
 
 function parseJSON(response) {
-  return response.json();
+    return response.json();
 }
 
 function checkStatus(response) {
-  if (response.status >= 200 && response.status < 300) {
-    return response;
-  }
-  const error = new Error(response.statusText);
-  error.response = response;
-  throw error;
+    if (response.status >= 200 && response.status < 300) {
+        return response;
+    }
+
+    const error = new Error(response.statusText);
+    error.response = response;
+    throw error;
+}
+
+export function requestData(url, options = {}) {
+    /**
+     * 返回一个 RestResult.data
+     * @param url
+     * @param options
+     * @returns {Promise.<TResult>}
+     */
+    const url1 = `${API_HOST}${url}`;
+
+    options.headers = {
+        user_token: 'e15fc3c18fecfd8e3937c2b8c8677b4a',
+        'Content-Type': 'application/json'
+    };
+    return fetch(url1, options)
+        .then(checkStatus)
+        .then(parseJSON)
+        .then(result => result.data)
+        .catch(err => ({err}));
 }
 
 
-class Headers {
-  constructor() {
-    this.headers = {};
-  }
+/**
+ * 返回一个 RestResult
+ *
+ * @param  {string} url       The URL we want to request
+ * @param  {object} [options] The options we want to pass to "fetch"
+ * @return {object}           An object containing either "data" or "err"
+ */
+export function request(url, options = {}) {
+    const url1 = `${API_HOST}${url}`;
 
-  getHeaders() {
-    this.headers = {
-      user_token: cookie.load('user_token'),
-      'Content-Type': 'application/json'
+    options.headers = {
+        user_token: 'e15fc3c18fecfd8e3937c2b8c8677b4a',
+        'Content-Type': 'application/json'
     };
 
-    return this.headers;
-  }
+    return fetch(url1, options)
+        .then(checkStatus)
+        .then(parseJSON)
+        .then(result => result)
+        .catch(err => ({err}));
 }
-
-/**
- * Requests a URL, returning a promise.
- *
- * @param  {string} url       The URL we want to request
- * @param  {object} [options] The options we want to pass to "fetch"
- * @return {object}           An object containing either "data" or "err"
- */
-export async function requestResult(url, options = {}) {
-  const env = process.env.NODE_ENV || 'dev';
-  let Header = new Headers();
-  options.headers = Header.getHeaders();
-
-  url = `${apiDomains[env]}${url}`;
-
-  const response = await fetch(url, options);
-  checkStatus(response);
-  const result = await response.json();
-
-  if (result.code == 105 || result.code == 104) {
-    browserHistory.push(ROUTE_USERS_SIGN_IN)
-  }
-
-  return result;
-}
-
-/**
- * Requests a URL, returning a promise.
- *
- * @param  {string} url       The URL we want to request
- * @param  {object} [options] The options we want to pass to "fetch"
- * @return {object}           An object containing either "data" or "err"
- */
-export async function request(url, options = {}) {
-  let Header = new Headers();
-  options.headers = Header.getHeaders();
-  const env = process.env.NODE_ENV || 'dev';
-  url = `${apiDomains[env]}${url}`;
-  const response = await fetch(url, options);
-  checkStatus(response);
-  const result = await response.json();
-  if (result.code != 1) {
-    if (result.code == 105 || result.code == 104) {
-      browserHistory.push(ROUTE_USERS_SIGN_IN)
-    }
-    throw new Error(`请求 url : ${url},返回结果 : ${result.desc}`);
-  }
-  return result.data;
-}
-
