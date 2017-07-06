552c7f37fe8b961d6d19376a1517cfba209e1c81
oyhk <381895649@qq.com>
2017-07-06 12:18:17 +0800

    fix

---
+++ mkfree-deploy-frontend/src/utils/Request.js
@@ -0,0 +1,58 @@
+import fetch from 'dva/fetch';
+
+function parseJSON(response) {
+    return response.json();
+}
+
+function checkStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+        return response;
+    }
+
+    const error = new Error(response.statusText);
+    error.response = response;
+    throw error;
+}
+
+export function requestData(url, options = {}) {
+    /**
+     * 返回一个 RestResult.data
+     * @param url
+     * @param options
+     * @returns {Promise.<TResult>}
+     */
+    const url1 = `${API_HOST}${url}`;
+
+    options.headers = {
+        user_token: 'e15fc3c18fecfd8e3937c2b8c8677b4a',
+        'Content-Type': 'application/json'
+    };
+    return fetch(url1, options)
+        .then(checkStatus)
+        .then(parseJSON)
+        .then(result => result.data)
+        .catch(err => ({err}));
+}
+
+
+/**
+ * 返回一个 RestResult
+ *
+ * @param  {string} url       The URL we want to request
+ * @param  {object} [options] The options we want to pass to "fetch"
+ * @return {object}           An object containing either "data" or "err"
+ */
+export function request(url, options = {}) {
+    const url1 = `${API_HOST}${url}`;
+
+    options.headers = {
+        user_token: 'e15fc3c18fecfd8e3937c2b8c8677b4a',
+        'Content-Type': 'application/json'
+    };
+
+    return fetch(url1, options)
+        .then(checkStatus)
+        .then(parseJSON)
+        .then(result => result)
+        .catch(err => ({err}));
+}
